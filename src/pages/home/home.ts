142a8487e158c26c934c32e168cf67ea17e232c9
sumanthio <sumanth@bytemark.co>
2018-05-21 19:12:06 +0530

    Added list view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {NewsProviderListPage} from "../news-provider-list/news-provider-list";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public NewsProviderListPage: any = NewsProviderListPage;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  next() {
+    console.log("Yasss");
+    this.navCtrl.push(NewsProviderListPage);
 
   }
 
